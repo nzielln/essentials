04005e1dd4f487562726732febb02e627eebb412
nzielln <ellah.nzikoba@gmail.com>
2019-04-26 15:36:50 -0400

    Updates

---
+++ precache-manifest.0abb35e011bbf3d3a9217879cb1225dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "35307bf37e59b59ff86fa47b641b553f",
+    "url": "/essentials/static/media/Antonio-Regular.35307bf3.woff"
+  },
+  {
+    "revision": "91bb46a5a890ca25a33abe19f0636f38",
+    "url": "/essentials/static/media/Antonio-Light.91bb46a5.woff"
+  },
+  {
+    "revision": "4e2c22dbb310737631c3d4825672dfa8",
+    "url": "/essentials/static/media/Antonio-Bold.4e2c22db.woff"
+  },
+  {
+    "revision": "7ab2004e6bf8f6ffcd06",
+    "url": "/essentials/static/js/runtime~main.c29ad370.js"
+  },
+  {
+    "revision": "0a2f68d08269c7d7df55",
+    "url": "/essentials/static/js/main.0e84f7aa.chunk.js"
+  },
+  {
+    "revision": "4c48990bedaf0cf4ee50",
+    "url": "/essentials/static/js/2.a852273e.chunk.js"
+  },
+  {
+    "revision": "0a2f68d08269c7d7df55",
+    "url": "/essentials/static/css/main.e5097e7b.chunk.css"
+  },
+  {
+    "revision": "7be9d7e9390a49a0745200413cd0b1e6",
+    "url": "/essentials/index.html"
+  }
+];
